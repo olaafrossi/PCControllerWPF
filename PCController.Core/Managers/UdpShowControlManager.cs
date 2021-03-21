6738271c63ae8603528850fa878dc4bbc6597ebd
Olaaf Rossi <olaaf.rossi@gmail.com>
2021-03-21 21:10:07 +0700

    fixes #2 major cleanup

--- PCController.Core/Managers/UdpShowControlManager.cs
+++ PCController.Core/Managers/UdpShowControlManager.cs
@@ -13,12 +13,9 @@ using ThreeByteLibrary.Dotnet.NetworkUtils;
 
 namespace PCController.Core.Managers
 {
-    public class UdpShowControlManager
+    public class UdpShowControlManager : IDisposable
     {
         private readonly IAsyncUdpLink _asyncUdpLink;
-
-
-        private IDisposable _disposableImplementation;
 
         public UdpShowControlManager(string IPAddress, int remotePort, int localPort)
         {
@@ -27,31 +24,14 @@ namespace PCController.Core.Managers
             _asyncUdpLink.DataReceived += LinkOnDataReceived;
         }
 
-
         public string IncomingMessage { get; set; }
-
-        public string OutgoingMessage { get; set; }
-
-        public string RemoteIP { get; set; }
-
-        public string LocalIP { get; set; } = GetLocalIPAddress();
-
-        public int LocalPort { get; set; }
-
-        public int RemotePort { get; set; }
-
-        public string Timestamp { get; set; }
-
-        public int UDPPort { get; set; }
 
         public string UdpFrameCombined { get; set; }
 
-        public event EventHandler UDPDataReceived;
-
-        public void DisposeUDPLink()
+        public void Dispose()
         {
-            _asyncUdpLink.Dispose();
-            // needs works
+            ReleaseUnmanagedResources();
+            GC.SuppressFinalize(this);
         }
 
         public void AddUdpFrame(string frame)
@@ -60,28 +40,34 @@ namespace PCController.Core.Managers
             {
                 string message = frame;
 
+                Log.Information("Going to send a message over the UDP driver {message}", message);
+
                 if (message.Contains("!0D!0A"))
                 {
                     message = message.Replace("!0D!0A", "\r\n");
                     byte[] inputBytes = Encoding.ASCII.GetBytes(message); // new byte array and feed it the input string
                     _asyncUdpLink.SendMessage(inputBytes);
+                    Log.Information("Message contained <crlf>, replaced the hex with ASCII to sent properly {message}", message);
                 }
                 else if (message.Contains("!0D"))
                 {
                     message = message.Replace("!0D", "\r");
                     byte[] inputBytes = Encoding.ASCII.GetBytes(message); // new byte array and feed it the input string
                     _asyncUdpLink.SendMessage(inputBytes);
+                    Log.Information("Message contained <cr>, replaced the hex with ASCII to sent properly {message}", message);
                 }
                 else if (message.Contains("!0A"))
                 {
                     message = message.Replace("!0A", "\n");
                     byte[] inputBytes = Encoding.ASCII.GetBytes(message); // new byte array and feed it the input string
                     _asyncUdpLink.SendMessage(inputBytes);
+                    Log.Information("Message contained <lf>, replaced the hex with ASCII to sent properly {message}", message);
                 }
                 else
                 {
                     byte[] inputBytes = Encoding.ASCII.GetBytes(message); // new byte array and feed it the input string
                     _asyncUdpLink.SendMessage(inputBytes);
+                    Log.Information("Message did not contain any ending characters {message}", message);
                 }
             }
             catch (Exception e)
@@ -92,53 +78,67 @@ namespace PCController.Core.Managers
             WriteUDPDataToDataBase(frame, true);
         }
 
+        public void LinkOnDataReceived(object sender, EventArgs e)
+        {
+            byte[] dataBytes = _asyncUdpLink.GetMessage();
+            string messageFromController = Encoding.ASCII.GetString(dataBytes); // new byte array and feed it the input string
+            Log.Information("Message from Remote {messageFromController}", messageFromController);
+            IncomingMessage = messageFromController;
+            WriteUDPDataToDataBase(string.Empty, false);
+            IncomingMessage = string.Empty; // clear the prop
+        }
+
+        public event EventHandler UDPDataReceived;
+
         public void WriteUDPDataToDataBase(string frameToSend, bool sentTypeMessage)
         {
-            SQLiteCRUD sql = new SQLiteCRUD(ConnectionStringManager.GetConnectionString(ConnectionStringManager.DataBases.Network));
-            UdpSenderModel udpFrame = new UdpSenderModel();
+            SQLiteCRUD sql = new(ConnectionStringManager.GetConnectionString(ConnectionStringManager.DataBases.Network));
+            UdpSenderModel udpFrame = new();
 
             if (sentTypeMessage is true)
             {
                 udpFrame.OutgoingMessage = frameToSend;
                 udpFrame.IncomingMessage = IncomingMessage;
                 udpFrame.RemoteIP = _asyncUdpLink.Address;
-                udpFrame.LocalIP = LocalIP;
+                udpFrame.LocalIP = GetLocalIPAddress();
                 udpFrame.LocalPort = _asyncUdpLink.LocalPort;
                 udpFrame.RemotePort = _asyncUdpLink.Port;
                 udpFrame.Timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
                 sql.InsertUdpSentData(udpFrame);
 
                 string udpFrameCombine =
-                    $"SENT: {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff} Sent Frame: {frameToSend} Remote IP: {_asyncUdpLink.Address} This IP: {LocalIP} Remote Port: {_asyncUdpLink.LocalPort} Local Port: {_asyncUdpLink.Port}";
-                //UDPRealTimeCollection.Insert(0, udpFrameCombine);
+                    $"SENT: {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff} Sent Frame: {frameToSend} Remote IP: {_asyncUdpLink.Address} This IP: {GetLocalIPAddress()} Remote Port: {_asyncUdpLink.LocalPort} Local Port: {_asyncUdpLink.Port}";
+
                 UdpFrameCombined = udpFrameCombine;
-                _asyncUdpLink.DataReceived += UDPDataReceived;
             }
             else
             {
                 udpFrame.OutgoingMessage = string.Empty;
                 udpFrame.IncomingMessage = IncomingMessage;
                 udpFrame.RemoteIP = _asyncUdpLink.Address;
-                udpFrame.LocalIP = LocalIP;
+                udpFrame.LocalIP = GetLocalIPAddress();
                 udpFrame.LocalPort = _asyncUdpLink.LocalPort;
                 udpFrame.RemotePort = _asyncUdpLink.Port;
                 udpFrame.Timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
                 sql.InsertUdpSentData(udpFrame);
 
                 string udpFrameCombine =
-                    $"RECEIVED: {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff} Received Frame: {IncomingMessage} Remote IP: {_asyncUdpLink.Address} This IP: {LocalIP} Remote Port: {_asyncUdpLink.LocalPort} Local Port: {_asyncUdpLink.Port}";
+                    $"RECEIVED: {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff} Received Frame: {IncomingMessage} Remote IP: {_asyncUdpLink.Address} This IP: {GetLocalIPAddress()} Remote Port: {_asyncUdpLink.LocalPort} Local Port: {_asyncUdpLink.Port}";
 
                 UdpFrameCombined = udpFrameCombine;
-                _asyncUdpLink.DataReceived += UDPDataReceived;
             }
+
+            Log.Information("created an event handler to propogate messages to the ViewModel");
+            _asyncUdpLink.DataReceived += UDPDataReceived;
         }
 
         private static string GetLocalIPAddress()
         {
             try
             {
-                var host = Dns.GetHostEntry(Dns.GetHostName());
-                foreach (var ip in host.AddressList)
+                Log.Logger.Information("Asking the base DNS class for IPv4 addresses");
+                IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+                foreach (IPAddress ip in host.AddressList)
                 {
                     if (ip.AddressFamily == AddressFamily.InterNetwork)
                     {
@@ -155,40 +155,20 @@ namespace PCController.Core.Managers
             return "No network adapters with an IPv4 address in the system!";
         }
 
-        public void LinkOnDataReceived(object sender, EventArgs e)
+        private void ReleaseUnmanagedResources()
         {
-            byte[] dataBytes = _asyncUdpLink.GetMessage();
-            string messageFromController = Encoding.ASCII.GetString(dataBytes); // new byte array and feed it the input string
-            Log.Information("Message from Remote {messageFromController}", messageFromController);
-            IncomingMessage = messageFromController;
-            WriteUDPDataToDataBase(string.Empty, false);
-            IncomingMessage = string.Empty; // clear the prop
+            Log.Information("Locking the UDP Driver");
+            lock (_asyncUdpLink)
+            {
+                _asyncUdpLink.Enabled = false;
+                _asyncUdpLink.Dispose();
+                Log.Information("Set UDP Driver Enable to false, and called the dispose method on the library");
+            }
         }
 
-        public void Dispose()
+        ~UdpShowControlManager()
         {
-            _asyncUdpLink.Enabled = false;
-            _asyncUdpLink.Dispose();
-            _disposableImplementation?.Dispose();
+            ReleaseUnmanagedResources();
         }
-    }
-
-    public class AsyncUdpLinkEvents : EventArgs
-    {
-        public string IncomingMessage { get; set; }
-
-        public string OutgoingMessage { get; set; }
-
-        public string RemoteIP { get; set; }
-
-        public string LocalIP { get; set; }
-
-        public int LocalPort { get; set; }
-
-        public int RemotePort { get; set; }
-
-        public string Timestamp { get; set; }
-
-        public int UDPPort { get; set; }
     }
 }
