b1e3d3a749e3837d4159ea08293369892b34c553
Olaaf Rossi <olaaf.rossi@gmail.com>
2021-03-25 14:32:39 +0700

    fixes #5 and resharper cleanup

--- PCController.Core/ViewModels/Navigation/PCNetworkListenerViewModel.cs
+++ PCController.Core/ViewModels/Navigation/PCNetworkListenerViewModel.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using MvvmCross;
-using MvvmCross.Base;
 using MvvmCross.Commands;
 using MvvmCross.Logging;
 using MvvmCross.Navigation;
@@ -15,36 +14,39 @@ using MvvmCross.ViewModels;
 using PCController.Core.Managers;
 using PCController.DataAccess;
 using PCController.DataAccess.Models;
-using ThreeByteLibrary.Dotnet;
 using ThreeByteLibrary.Dotnet.NetworkUtils;
+// ReSharper disable CheckNamespace
+// ReSharper disable once ArrangeModifiersOrder
 
 namespace PCController.Core.ViewModels
 {
-    public class PCNetworkListenerViewModel : MvxNavigationViewModel<WindowChildParam>
+    public sealed class PCNetworkListenerViewModel : MvxNavigationViewModel<WindowChildParam>
     {
-        private readonly Stopwatch stopwatch;
-
-        public Stopwatch NetStopwatch = new Stopwatch();
-
+        private readonly Stopwatch _stopwatch;
         private WindowChildParam _param;
+        public Stopwatch NetStopwatch = new();
 
-        public PCNetworkListenerViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService) :
-            base(logProvider, navigationService)
+        public PCNetworkListenerViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService) : base(logProvider, navigationService)
         {
+            Log.Info("PCNetworkListenerViewModel has been constructed {logProvider} {navigationService}", logProvider, navigationService);
+
+            // Setup UI Commands
             RefreshNetMsgCommand = new MvxCommand(GetNetLogs);
 
-            Serilog.Log.Logger.Information("PCNetworkListenerViewModel has been constructed {logProvider} {navigationService}", logProvider, navigationService);
+            // Fetch Initial Data
+            _stopwatch = new Stopwatch();
+            GetNetLogs();
 
+            // get singleton and create event handler
             IPcNetworkListener pcNetworkListener = Mvx.IoCProvider.Resolve<IPcNetworkListener>();
-            
-            int listeningPort = pcNetworkListener.GetAppSettingsDataUdpPort();
-            ListeningUDPPort = listeningPort.ToString();
-
             pcNetworkListener.MessageHit += PCNetworkManagerOnMessage;
 
-            stopwatch = new Stopwatch();
-            GetNetLogs();
+            // set initial UI Fields
+            int listeningPort = pcNetworkListener.GetAppSettingsDataUdpPort();
+            ListeningUDPPort = listeningPort.ToString();
         }
+
+        public IMvxCommand RefreshNetMsgCommand { get; set; }
 
         public string ListeningUDPPort { get; set; }
 
@@ -62,55 +64,33 @@ namespace PCController.Core.ViewModels
 
         public string DataBaseQueryTime { get; set; }
 
-        public IMvxCommand RefreshNetMsgCommand { get; set; }
-
         public string NumberOfNetMsgToFetch { get; set; }
 
-        public int ParentNo => _param.ParentNo;
-        public string Text => $"I'm No.{_param.ChildNo}. My parent is No.{_param.ParentNo}";
-
-        private void PCNetworkManagerOnMessage(object sender, NetworkMessagesEventArgs e)
+        public int ParentNo
         {
-            NetStopwatch.Start();
-            
-            PCNetworkManager pc = new PCNetworkManager(e);
-
-            RemoteControlIP = e.RemoteIP;
-            RaisePropertyChanged(() => RemoteControlIP);
-            
-            RemoteControlPort = e.RemotePort;
-            RaisePropertyChanged(() => RemoteControlPort);
-           
-            RemoteControlTimeStamp = e.Timestamp;
-            RaisePropertyChanged(() => RemoteControlTimeStamp);
-            
-            RemoteControlLastMessage = e.IncomingMessage;
-            RaisePropertyChanged(() => RemoteControlLastMessage);
-            
-            string t = $"{NetStopwatch.ElapsedMilliseconds.ToString()} ms";
-            RemoteControlTimeSinceLastMessage = t;
-            RaisePropertyChanged(() => RemoteControlTimeSinceLastMessage);
-            
-            NetStopwatch.Stop();
-            NetStopwatch.Reset();
-
+            get
+            {
+                return _param.ParentNo;
+            }
         }
 
-        private async Task StartNetWorkTimer()
+        public string Text
         {
-            
-            do
+            get
             {
+                return $"I'm No.{_param.ChildNo}. My parent is No.{_param.ParentNo}";
+            }
+        }
 
-            } 
-            while (NetStopwatch.IsRunning);
-
+        public override void Prepare(WindowChildParam param)
+        {
+            _param = param;
         }
 
         private void GetNetLogs()
         {
-            stopwatch.Start();
-            SQLiteCRUD sql = new SQLiteCRUD(ConnectionStringManager.GetConnectionString(ConnectionStringManager.DataBases.Network));
+            _stopwatch.Start();
+            SQLiteCRUD sql = new(ConnectionStringManager.GetConnectionString(ConnectionStringManager.DataBases.Network));
             int numOfMsgs = 20;
 
             try
@@ -149,17 +129,44 @@ namespace PCController.Core.ViewModels
                 Serilog.Log.Logger.Error("Didn't parse the number in the Net ComboBox {numOfMsgs}", numOfMsgs, e);
             }
 
-            Serilog.Log.Logger.Information("Getting Data Logs{numOfMsgs}", numOfMsgs);
-            var rows = sql.GetSomeNetData(numOfMsgs);
+            Log.Info("Getting Data Logs{numOfMsgs}", numOfMsgs);
+            IList<NetworkMessageModel> rows = sql.GetSomeNetData(numOfMsgs);
             NetGridRows = rows;
 
-            stopwatch.Stop();
-            string timeToFetchFromDB = $" DB query time: {stopwatch.ElapsedMilliseconds} ms";
-            DataBaseQueryTime = timeToFetchFromDB;
+            _stopwatch.Stop();
+            string timeToFetchFromDb = $" DB query time: {_stopwatch.ElapsedMilliseconds} ms";
+            DataBaseQueryTime = timeToFetchFromDb;
             RaisePropertyChanged(() => NetGridRows);
             RaisePropertyChanged(() => DataBaseQueryTime);
         }
 
-        public override void Prepare(WindowChildParam param) => _param = param;
+        private void PCNetworkManagerOnMessage(object sender, NetworkMessagesEventArgs e)
+        {
+            NetStopwatch.Start();
+            PCNetworkManager pc = new(e);
+
+            RemoteControlIP = e.RemoteIP;
+            RemoteControlPort = e.RemotePort;
+            RemoteControlTimeStamp = e.Timestamp;
+            RemoteControlLastMessage = e.IncomingMessage;
+            string time = $"{NetStopwatch.ElapsedMilliseconds} ms";
+            RemoteControlTimeSinceLastMessage = time;
+
+            RaisePropertyChanged(() => RemoteControlIP);
+            RaisePropertyChanged(() => RemoteControlPort);
+            RaisePropertyChanged(() => RemoteControlTimeStamp);
+            RaisePropertyChanged(() => RemoteControlLastMessage);
+            RaisePropertyChanged(() => RemoteControlTimeSinceLastMessage);
+
+            NetStopwatch.Stop();
+            NetStopwatch.Reset();
+        }
+
+        private async Task StartNetWorkTimer()
+        {
+            do
+            {
+            } while (NetStopwatch.IsRunning);
+        }
     }
 }
